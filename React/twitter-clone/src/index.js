e9a8c74fc003c1db17f9f79af283406328817e47
KKG <kkg05102@gmail.com>
2022-04-07 04:09:15 +0900

    twitter-clone #1

---
+++ React/twitter-clone/src/index.js
@@ -0,0 +1,13 @@
+import React from "react";
+// import ReactDOM from 'react-dom';
+import { createRoot } from "react-dom/client";
+import App from "components/App";
+
+createRoot(document.getElementById("root")).render(<App />);
+
+// ReactDOM.render(
+//   <React.StrictMode>
+//     <App />
+//   </React.StrictMode>,
+//   document.getElementById('root')
+// );
